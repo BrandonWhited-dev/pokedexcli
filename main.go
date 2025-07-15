eab39fc8c49b811190110f2b9cc62c6b57d3211c
brandonwhited-dev <brandon.education@proton.me>
2025-07-15 17:21:43 -0400

    made the basic repl to accept a command

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		fmt.Printf("Your command was: %s \n", cleanInput(scanner.Text())[0])
+	}
 }
 
 // the function will take the string entered and break it up into seperate words
 func cleanInput(text string) []string {
-	return strings.Split(text, " ")
+	return strings.Split(strings.ToLower(text), " ")
 }
