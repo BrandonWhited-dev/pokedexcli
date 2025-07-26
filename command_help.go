0b980abc28744a5c83a673b7dd7944e0771e2c93
brandonwhited-dev <brandon.education@proton.me>
2025-07-26 15:27:52 -0400

    updated help command

--- command_help.go
+++ command_help.go
@@ -6,9 +6,12 @@ func commandHelp(*config, []string) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usuage:")
 	fmt.Println()
+	fmt.Println("anything in [] means a variable you enter")
+	fmt.Println()
 	fmt.Println("help: Displays a help message")
 	fmt.Println("map: Displays next location information")
 	fmt.Println("mapb: Displays previous location information")
 	fmt.Println("exit: Exit the Pokedex")
+	fmt.Println("explore [location name]: Displays the pokemon in an area")
 	return nil
 }
