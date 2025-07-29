508c211f6ed9dc7b3128336332a66d10fc5a7ddd
brandonwhited-dev <brandon.education@proton.me>
2025-07-29 03:44:38 -0400

    Read data function reads pokedex.json from documents folder

---
+++ storage.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func ReadData(cfg *config) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Error getting home file path")
+		return
+	}
+	path := filepath.Join(home, "Documents", "pokedex.json")
+	storedData, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Println("There is no pervious save data...")
+		return
+	}
+	data := make(map[string]PokemonInfo)
+	if err := json.Unmarshal(storedData, &data); err != nil {
+		fmt.Println("Save data corrupted...")
+		return
+	}
+	cfg.Storage = data
+}
+
