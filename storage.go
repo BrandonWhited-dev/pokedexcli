ab92795f3258df62b7c9e393a842c6803ea1b78b
brandonwhited-dev <brandon.education@proton.me>
2025-07-29 03:44:52 -0400

    writes to home/Documents/pokedex.json

--- storage.go
+++ storage.go
@@ -27,3 +27,20 @@ func ReadData(cfg *config) {
 	cfg.Storage = data
 }
 
+func WriteData(cfg *config) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Error getting home file path")
+		return
+	}
+	path := filepath.Join(home, "Documents", "pokedex.json")
+	fmt.Println("Writing to " + path)
+	data, err := json.Marshal(cfg.Storage)
+	if err != nil {
+		return
+	}
+	err = os.WriteFile(path, data, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
